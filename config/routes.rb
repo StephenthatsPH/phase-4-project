d88e1b1a505f51b0a87fe081633edb332023adcb
Stephen <stephen.a.nerby@gmail.com>
2023-02-14 14:11:11 -0500

    React setup

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   
+  # route to test your configuration
+  get '/hello', to: 'application#hello_world'
 end
