94c0a208144432696d306410623b5d4fc8593d1d
Stephen <stephen.a.nerby@gmail.com>
2023-02-22 17:48:36 -0500

    before_action :authorized uncomment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-    # before_action :authorized
+    before_action :authorized
     
     private
-    
+
     def hello_world
         session[:count] = (session[:count] || 0) + 1
         render json: { count: session[:count] }
