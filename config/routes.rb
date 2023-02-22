42921d27a983ba51316ec1390c1de5bd6714f447
Stephen <stephen.a.nerby@gmail.com>
2023-02-22 17:42:00 -0500

    Namespace api

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
 Rails.application.routes.draw do
-  # resources :users
-  resources :reviews
-  resources :programs
-  # resources :programs, except: [:edit, :new]
-  # resources :reviews, only: [:index, :post, :patch, :delete]
-  # route to test your configuration
-  get '/hello', to: 'application#hello_world'
-  post '/signup', to: 'users#create'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-  get '/me', to: 'users#show'
+  namespace :api do
+    resources :reviews
+    resources :programs
+    # resources :users
+    # resources :programs, except: [:edit, :new]
+    # resources :reviews, only: [:index, :post, :patch, :delete]
+    # route to test your configuration
+    get '/hello', to: 'application#hello_world'
+    post '/signup', to: 'users#create'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
+    get '/me', to: 'users#show'
+  end
   
-  get '*path',to: 'fallback#index',
-  constraints: ->(req) { !req.xhr? && req.format.html? }
+  get '*path',to: 'fallback#index', constraints: ->(req) { !req.xhr? && req.format.html? }
 end
 
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
