c2af5ccfa1cf1563ebaa54b73455e6ede19665de
Stephen <stephen.a.nerby@gmail.com>
2023-03-11 11:42:50 -0500

    changed validations

--- app/models/program.rb
+++ app/models/program.rb
@@ -5,9 +5,9 @@ class Program < ApplicationRecord
     validates :name, presence: true, uniqueness: true
     validates :hospital, presence: true
     validates :website, presence: true
-    validates :pgy1salary, presence: true, length: {minimum: 1, maximum:500000}
-    validates :program_age, presence: true, length: {minimum: 1, maximum:100}
-    validates :program_size, presence: true, length: {minimum: 1, maximum:100}
+    validates :pgy1salary, presence: true
+    validates :program_age, presence: true
+    validates :program_size, presence: true
     validates :specialty, presence: true
     validates :state, presence: true, length: {maximum: 2}
     validates :area_type, presence: { message: ': Is the program in a urban, suburban, or rural area?' }
