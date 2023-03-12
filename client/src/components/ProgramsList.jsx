fbd9b1e6e0aa25a08164e448619c4e2cc15d4b15
Stephen <stephen.a.nerby@gmail.com>
2023-03-11 19:47:57 -0500

    Program list indent, specialty display

--- client/src/components/ProgramsList.jsx
+++ client/src/components/ProgramsList.jsx
@@ -5,16 +5,19 @@ function ProgramsList({ programs }) {
 
     const getPrograms = programs.map((program) => {
         return <div key={program.id}>
-            <h2>{program.name}</h2>
+            <h3>{program.name}</h3>
+            <ul>
+            {program.specialty}
             <NavLink to={`/programs/${program.id}/overview`}>
                 <p>More info on {program.name}</p>
             </NavLink>
+            </ul>
         </div>
     })
 
     return (
         <div>
-            <h1>Programs</h1>
+            <h1>Teaching Programs</h1>
             {getPrograms}
         </div>
     );
